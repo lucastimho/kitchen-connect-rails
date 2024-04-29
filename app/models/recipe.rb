9f8af3033b9497716d78e10daf1f71220e83652c
lucastimho <lucas.t.ho.lh@gmail.com>
2024-04-28 21:34:15 -0500

    adds model functions

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,20 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true
+
+  def i_list
+    ingredients.split(", ")
+  end
+
+  def d_list
+    directions.split(", ")
+  end
+
+  def legible_created_at
+    created_at.strftime("%A, %d %b %Y %l:%M %p")
+  end
+
+  def t_list
+    tags.split(", ")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :recipes
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :name, length: { maximum: 20 }
+  validates :password, length: { in: 6..20 }
 end
