d75f2f1c5bf9a4ba59dac78a5551be9426ba9dc4
lucastimho <lucas.t.ho.lh@gmail.com>
2024-04-28 22:25:06 -0500

    adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "/recipes" => "recipes#index"
+  get "/recipes/:id" => "recipes#show"
+  post "/recipes" => "recipes#create"
+  patch "/recipes/:id" => "recipes#update"
+  delete "/recipes/:id" => "recipes#destroy"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  post "/users" => "users#create" #signup as a user
+
+  post "/sessions" => "sessions#create" #log in as a user
 end
