bec47897ef42dc8a980b1c5848c3f6212df9303e
lucastimho <lucas.t.ho.lh@gmail.com>
2024-04-28 23:08:37 -0500

    adds many to many relations to model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-User.create!([
-  {name: "Lucas", email: "lucas@example.com", password_digest: "$2a$12$de7yMXmpwEbvIDvmbqOQo.FL4s9xmXHbuB3dw.l8xRwoGmzRigNV2"},
-  {name: "Ethan", email: "ethan@example.com", password_digest: "$2a$12$8Hy19ojah17QkP/ey1RmOuapTwkGvg91u83VD7p3yu5anQdfCNIEa"}
-])
+# User.create!([
+#   {name: "Lucas", email: "lucas@example.com", password_digest: "$2a$12$de7yMXmpwEbvIDvmbqOQo.FL4s9xmXHbuB3dw.l8xRwoGmzRigNV2"},
+#   {name: "Ethan", email: "ethan@example.com", password_digest: "$2a$12$8Hy19ojah17QkP/ey1RmOuapTwkGvg91u83VD7p3yu5anQdfCNIEa"}
+# ])
 
 Recipe.create!([
-  {title: "Coconut Cakes", chef: "Lucas Ho", ingredients: "Coconuts, cake", directions: "Put coconuts and cake together, bake", user_id: 1},
-  {title: "Salmon Sashimi", chef: "Ethan Le", ingredients: "Salmon", directions: "Sashimi the salmon", user_id: 1},
-  {title: "Sandwich", chef: "Brandan Robertciello", ingredients: "Two slices of bread", directions: "Place slices of bread next to each other.", user_id: 1},
-  {title: "Nerds", chef: "Pierre Garcia", ingredients: "Nerds candy", directions: "Buy licorice from store.", user_id: 1},
-  {title: "Creme Pie", chef: "Jesse Crauthers", ingredients: "Flour, water, creme.", directions: "Mix flour and water, add creme, bake at 325 degrees for 1 hour.", user_id: 1},
-  {title: "Pizza", chef: "Henry Ho", ingredients: "Flour, water, pepperoni, cheese", directions: "Mix flour and water, add cheese and pepperoni, shape as a circle, bake in oven.", user_id: 1},
-  {title: "Corn on the cob", chef: "Nikhil Mathew", ingredients: "Corn", directions: "Place corn on the cob, grill for 10 minutes", user_id: 1}
+  {title: "Coconut Cakes", author: "Lucas Ho", ingredients: "Coconuts, cake", directions: "Put coconuts and cake together, bake", user_id: 1},
+  {title: "Salmon Sashimi", author: "Ethan Le", ingredients: "Salmon", directions: "Sashimi the salmon", user_id: 1},
+  {title: "Sandwich", author: "Brandan Robertciello", ingredients: "Two slices of bread", directions: "Place slices of bread next to each other.", user_id: 1},
+  {title: "Nerds", author: "Pierre Garcia", ingredients: "Nerds candy", directions: "Buy licorice from store.", user_id: 1},
+  {title: "Creme Pie", author: "Jesse Crauthers", ingredients: "Flour, water, creme.", directions: "Mix flour and water, add creme, bake at 325 degrees for 1 hour.", user_id: 1},
+  {title: "Pizza", author: "Henry Ho", ingredients: "Flour, water, pepperoni, cheese", directions: "Mix flour and water, add cheese and pepperoni, shape as a circle, bake in oven.", user_id: 1},
+  {title: "Corn on the cob", author: "Nikhil Mathew", ingredients: "Corn", directions: "Place corn on the cob, grill for 10 minutes", user_id: 1}
 ])
